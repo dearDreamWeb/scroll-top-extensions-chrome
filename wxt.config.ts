c5f9cc3ade609a721559e67f75a5bb98f3afca23
wangxiaobao <superwang9527@163.com>
2024-08-09 14:28:09 +0800

    feat: added tabs permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['storage'],
+    permissions: ['storage', 'tabs'],
   },
   vite() {
     return {
